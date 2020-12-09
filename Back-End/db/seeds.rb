6654db7f2238173b6f123d31532a1ec0a3e7fcfb
Rx8boi <60455199+Rx8boi@users.noreply.github.com>
2020-12-08 23:34:05 -0600

    Creating Player seeds

--- Back-End/db/seeds.rb
+++ Back-End/db/seeds.rb
@@ -5,13 +5,143 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-32.times do
 
-    player= Player.new(
-    name: Faker::Sports::Basketball.player,
-    team: Faker::Sports::Basketball.team,
-    position: Faker::Sports::Basketball.position,
-    )
+Player.create([
+{
+    name: "Ben Simmons",
+    team: "Philadelphia 76ers"
+    position: "Point Guard"
+    image: "./player_img/simmobe01.jpg"
+    injured: false
+    fantasy_points: 57
+
+}
+ {
+     name:"Pascal Siakam"
+     team:"Toronto Raptors"
+     position:"Power Forward"
+     image: "./player_img/siakapa01.jpg"
+     injured: false
+     fantasy_points:60
+
+}
+ {
+     name:"Bradley Beal"
+     team:"Washington Wizards"
+     position:
+     image:"./player_img/.jpg"
+     injured: false
+     fantasy_points:
+
+}
+ {
+     name:"Jayson Tatum"
+     team: "Boston Celtics"
+     position:
+     image:"./player_img/.jpg"
+     injured: false
+     fantasy_points:
+
+}
+ {
+     name:"Trae Young"
+     team:"Atlanta Hawks"
+     position:
+     image:"./player_img/.jpg"
+     injured: false
+     fantasy_points:
+
+}
+ {
+     name:"Russell Westbrook"
+     team:"Houston Rockets"
+     position:
+     image:"./player_img/.jpg"
+     injured: false
+     fantasy_points:
+
+}
+ {
+     name:"Joel Embiid"
+     team:"Philadelphia 76ers"
+     position:
+     image:"./player_img/.jpg"
+     injured: false
+     fantasy_points:
+
+}
+ {
+    name:"Damian Lillard"
+    team:"Portland Trail Blazers"
+    position:
+    image:"./player_img/.jpg"
+    injured: false
+    fantasy_points:
     
-    player.save
-end
+}
+ {
+     name:"Nikola Jokic"
+     team:"Denver Nuggets"
+     position:
+     image:"./player_img/.jpg"
+     injured: false
+     fantasy_points:
+
+}
+ {
+     name:"Anthony Davis"
+     team:"Los Angeles Lakers"
+     position:"Center"
+     image:"./player_img/davisan02.jpg"
+     injured: false
+     fantasy_points:62
+
+
+}
+ {
+     name:"Luka Doncic"
+     team:"Dallas Mavericks"
+     position:"Guard"
+     image:"./player_img/doncilu01.jpg"
+     injured: false
+     fantasy_points:61
+
+}
+{
+    name:"Kawhi Leonard"
+    team:"Los Angeles Clippers"
+    position:"Small Forward"
+    image:"./player_img/leonaka01.jpg"
+    injured: false
+    fantasy_points:57
+
+}
+ {
+     name:"James Harden"
+     team:"Houston Rockets"
+     position:"Guard"
+     image:"./player_img/hardeja01.jpg"
+     injured: false
+     fantasy_points:68
+
+}
+ {
+     name:"Giannis Antetokounmpo"
+     team:"Milwaukee Bucks"
+     position:"Forward/Guard"
+     image:"./player_img/antetgi01.jpg"
+     injured: false
+     fantasy_points:63
+
+}
+{
+    name:"LeBron James"
+    team:"Los Angeles Lakers"
+    position:"Forward/Guard"
+    image:"./player_img/jamesle01.jpg"
+    injured: false
+    fantasy_points:67
+
+}
+])
+
