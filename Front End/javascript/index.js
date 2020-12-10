43518ba03aa1b0b1fa93476f3570baed87c5e20a
Rx8boi <60455199+Rx8boi@users.noreply.github.com>
2020-12-10 03:25:36 -0600

    created front end files
    ALso started working on show.js & index.html

---
+++ Front End/javascript/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // do whatever has to happen first in your application
+    // need to load in "undrafted players"
+
+    API.addPlayers()
+})
